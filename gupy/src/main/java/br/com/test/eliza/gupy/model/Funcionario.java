fa475355f2e4e1d440a438849b593db6620037ba
elizapimentel <elizapimentel@hotmail.com>
2024-06-27 18:07:51 -0300

    add pessoa and funcionario models

---
+++ gupy/src/main/java/br/com/test/eliza/gupy/model/Funcionario.java
@@ -0,0 +1,27 @@
+package br.com.test.eliza.gupy.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@Entity
+public class Funcionario extends Pessoa {
+
+    private BigDecimal salario;
+    private String funcao;
+
+
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+}
---
+++ gupy/src/main/java/br/com/test/eliza/gupy/model/Pessoa.java
@@ -0,0 +1,31 @@
+package br.com.test.eliza.gupy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+}
